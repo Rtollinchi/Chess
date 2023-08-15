a6d7278c5addadc822aa87134edbfc0108f1fc2d
Rtollinchi <rtollinchi23@gmail.com>
2023-08-15 15:35:11 -0400

    chess board styling finished

--- app.js
+++ app.js
@@ -66,16 +66,30 @@ const startPieces = [
   queen,
   bishop,
   knight,
-  rook
-]
+  rook,
+];
 
 function createBoard() {
-  startPieces.forEach((startPiece) => {
+  startPieces.forEach((startPiece, i) => {
     const square = document.createElement("div");
     square.classList.add("square");
-    square.classList.add("beige");
+    square.innerHTML = startPiece
+    square.setAttribute("square-id", i)
+    // square.classList.add("beige");
+    const row = Math.floor( (63 - i) / 8) + 1
+    if (row % 2 === 0) {
+      square.classList.add(i % 2 === 0 ? "beige" : "brown")
+    } else {
+      square.classList.add(i % 2 === 0 ? "brown" : "beige")
+    }
+    if (i <= 15) {
+      square.firstChild.firstChild.classList.add("black")
+    }
+    if (i >= 48) {
+      square.firstChild.firstChild.classList.add("white")
+    }
     gameBoard.append(square);
   });
 }
 
-createBoard()
+createBoard();
