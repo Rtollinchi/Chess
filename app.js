de95098a692f58cc339bbc3231ec4cf247152453
Rtollinchi <rtollinchi23@gmail.com>
2023-08-22 13:33:47 -0400

    added check vaild move function and correct go function.  Both working

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const gameBoard = document.querySelector("#gameboard");
 const playerDisplay = document.querySelector("#player");
 const infoDisplay = document.querySelector("#info-display");
 const width = 8;
+let playerGo = "black"
+playerDisplay.textContent = "black"
 const startPieces = [
   rook,
   knight,
@@ -98,7 +100,7 @@ createBoard();
 
 
 
-const allSquares = document.querySelectorAll("#gameboard .square")
+const allSquares = document.querySelectorAll(".square")
 
 allSquares.forEach(square => {
   square.addEventListener("dragstart", dragStart)
@@ -120,5 +122,72 @@ function dragOver(e) {
 
 function dragDrop(e) {
   e.stopPropagation()
-  e.target.append(draggedElement)
+  const correctGo = draggedElement.firstChild.classList.contains(playerGo)
+  const taken = e.target.classList.contains("piece")
+  const valid = checkIfValid(e.target)
+  const opponentGo = playerGo === "white" ? "black" : "white";
+  const takenByOpponent = e.target.firstElementChild?.classList.contains(opponentGo)
+
+
+  if (correctGo) {
+    //must check this first
+    if (takenByOpponent && valid) {
+       e.target.parentNode.append(draggedElement)
+      e.target.remove()
+      changePlayer()
+      return
+    }
+    //then check this
+    if (taken && !takenByOpponent) {
+      infoDisplay.textContent = "invalid move!";
+      setTimeout(() => infoDisplay.textContent = "", 2000)
+      return
+    }
+    if (valid) {
+      e.target.append(draggedElement)
+      changePlayer()
+      return
+    }
+  }
+
+  function checkIfValid(target) {
+    const targetId = Number(target.getAttribute("square-id")) || Number(target.parentNode.getAttribute("square-id"))
+    const startId = Number(startPositionId)
+    const piece = draggedElement.id
+    console.log("targetId", targetId)
+    console.log("startId", startId)
+    console.log("piece", piece)
+
+    switch(piece) {
+      case "pawn" :
+        const starterRow = [8, 9, 10, 11, 12, 13, 14, 15]
+        if (starterRow.includes(startId) && startId + width * 2 === targetId || startId + width === targetId || startId + width -1 === targetId) {
+        return true
+ }
+    }
+  }
+
+  // e.target.append(draggedElement)
+  changePlayer();
 }
+ function changePlayer() {
+  if (playerGo === "black") {
+    reverseIds()
+    playerGo = "white";
+    playerDisplay.textContent = "white"
+  } else {
+    reverseIds()
+    playerGo = "black"
+    playerDisplay.textContent = "black"
+  }
+ }
+
+ function reverseIds() {
+  const allSquares = document.querySelectorAll(".square")
+  allSquares.forEach((square, i) => square.setAttribute("square-id", (width * width -1) - i))
+ }
+
+ function revertIds() {
+  const allSquares = document.querySelectorAll(".square")
+  allSquares.forEach((square, i) => square.setAttribute("square-id", i))
+ }
